5c9883453fd45b0eae600661a9b35c1b32a1523d
NikhileshChaudhary2903 <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2018-03-03 22:04:52 -0800

    Updated Client.py

--- quizzes/quizz1/client.py
+++ quizzes/quizz1/client.py
@@ -1,22 +1,20 @@
-import time
 import grpc
-import ping_pb2
 import ping_pb2_grpc
-class PingClient():
-    def _init_(self,host='0.0.0.0',port=3000):
-        self.channel=grpc.insecure.channel('%s %d' %(host,port))
-        self.stub=ping_pb2_grpc.PingPongStub(self.channel)
+from ping_pb2 import Request, Response
 
-    def ping(self,data):
-        req=Request(data=str(data))
+class PingClient():
+    def __init__(self, host='0.0.0.0', port=3000):
+        self.channel = grpc.insecure_channel('%s:%d' % (host, port))
+        self.stub = ping_pb2_grpc.PingPongStub(self.channel)
+    
+    def ping(self, data):
+        req = Request(data=str(data))
         return self.stub.ping(req)
 
-    def test():
-        client=PingClient()
-        resp=client.ping('ping')
-        print('Response={}',format(resp.data)) 
+def test():
+    client = PingClient()
+    resp = client.ping("ping")
+    print("Response = {}".format(resp.data))
 
-if _name_== '__main__':
-test()
-
-         
+if __name__ == '__main__':
+    test()
